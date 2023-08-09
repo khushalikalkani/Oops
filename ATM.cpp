43a7001da6147cdb673ac35db3c63ea55d3fff0d
dasrhan devani <devanidarshan333@gmail.com>
2023-08-09 19:43:31 +0530

    09-08-23

--- ATM.cpp
+++ ATM.cpp
@@ -1,37 +1,66 @@
 #include<iostream>
 using namespace std;
 class ATM{
-    int a = 1000;
-    int credited(){
-        cout<<"Enter a credit value: "<<endl;
-        cin>>a;
+    public:
+
+    int balance = 1000, Accno;
+    float deposit, withdraw, display;
+    string name;
+
+    void account(){
+        cout<<"Enter your account number: "<<endl;
+        cin>>Accno;
+
+        cout<<"Enter account holder number: "<<endl;
+        cin>>name;
+    }
+    void _deposit(){
+        cout<<"\n"<<"Enter deposit amount: "<<endl;
+        cin>>deposit;
+        balance += deposit;
+
+    }
+    void _withdraw(){
+        cout<<"\n"<<"Enter withdraw amount: "<<endl;
+        cin>>withdraw;
+        if(balance >= withdraw)
+        {
+            balance -= withdraw;
+        }else{
+            cout<<"sorry less balance in your account"<<"\n"<<endl;
+        }
+    }
+    void _display(){
+        cout<<"Your Account balance is: "<<balance<<"\n"<<endl;
     }
 
 };
 int main(){
     ATM t;
+    t.account();
     int x;
     do{
-        cout<<"1. credit"<<endl;
+        cout<<"\n"<<"1. credit"<<endl;
         cout<<"2. withdraw"<<endl;
         cout<<"3. showbalance"<<endl;
+        cout<<"4. exit"<<endl;
         
         cout<<"Enter your choice: "<<endl;
         cin>>x;
 
         switch(x){
             case 1:
-                t.credited();
+                t._deposit();
             break;
             case 2:
-                withdrew();
+                t._withdraw();
             break;
              case 3:
-                showbalance();
+                t._display();
             break;
              case 4:
             break;
 
         }
-    }while(x!=0);
+    }while(x!=4);
 }
